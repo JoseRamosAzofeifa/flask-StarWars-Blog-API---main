5310bf20b16cf498f50419f9d6e7c3ee186f72bc
JoseRamosAzofeifa <jara.zff@gmail.com>
2021-03-05 20:32:49 +0000

    StarWards-Blog-api version 1

--- src/main.py
+++ src/main.py
@@ -8,13 +8,18 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Usuario,Planeta,Personajes,Favoritos
+from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+jwt = JWTManager(app)
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -30,15 +35,215 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    query = Personajes.query.all()
 
+    # map the results and your list of people  inside of the all_people variable
+    results = list(map(lambda personaje: personaje.serialize(), query))
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "message": results
     }
 
     return jsonify(response_body), 200
 
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_one_single_people(people_id):
+    
+    query = Personajes.query.get(people_id)
+
+    # map the results and your list of people  inside of the all_people variable
+    results = query.serialize()
+    
+    response_body = {
+        "message": results
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    query = Planeta.query.all()
+
+    # map the results and your list of people  inside of the all_people variable
+    results = list(map(lambda Planeta: Planeta.serialize(), query))
+    response_body = {
+        "message": results
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_one_single_planet(planet_id):
+    
+    query = Planeta.query.get(planet_id)
+
+    # map the results and your list of people  inside of the all_people variable
+    results = query.serialize()
+    
+    response_body = {
+        "message": results
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    query = Usuario.query.all()
+
+    # map the results and your list of people  inside of the all_people variable
+    results = list(map(lambda usuario: usuario.serialize(), query))
+    response_body = {
+        "message": results
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/users/favorites', methods=['GET'])
+@jwt_required()
+def get_all_favoritos_id():
+    
+    user_id = get_jwt_identity()
+
+    favoritos_ID = Favoritos.query.filter_by(usuario_id=user_id)
+    # map the results and your list of people  inside of the all_people variable
+    results = list(map(lambda fab_id: fab_id.serialize(), favoritos_ID))
+    print (results)
+    for result in results:
+        if result.get("planeta_id") == None:
+            query_personajes = Personajes.query.get(result.get("personajes_id"))
+            print (query_personajes,"querypersonajes")
+            print (result,"resultados")
+            result["nombre"] = query_personajes.serialize().get("nombre")
+        else:
+            query_planetas = Planeta.query.get(result.get("planeta_id"))
+            result["nombre"] = query_planetas.serialize().get("nombre")
+    response_body = {
+        "message": results
+    }
+
+    return jsonify(response_body), 200
+
+
+@app.route('/users/favorites', methods=['POST'])
+@jwt_required()
+def agrega_favorito():
+    user_id = get_jwt_identity()
+    # recibir info del request
+    request_favorito = request.get_json()
+    print(request_favorito)
+    nuevo_favorito = Favoritos(usuario_id=user_id,planeta_id=request_favorito.get("planeta_id"),personajes_id=request_favorito.get("personajes_id"))
+    
+    db.session.add(nuevo_favorito)
+    db.session.commit()
+
+    return jsonify("favorito agregado satisfactoriamente"), 200
+
+@app.route('/favorite/<int:favorite_id>', methods=['DELETE'])
+@jwt_required()
+def borra_favorito(favorite_id):
+    
+    user_id = get_jwt_identity()
+
+    borra_favorito = Favoritos.query.get(favorite_id)
+    
+         
+    if borra_favorito is None:
+        raise APIException('Favorite not found', status_code=404)
+    
+    if  borra_favorito.serialize().get("usuario_id")!=user_id:
+        raise APIException('Usuario no autorizado', status_code=404)
+
+    db.session.delete(borra_favorito)
+
+    db.session.commit()
+
+    return jsonify("Favorito borrado satisfactoriamente"), 200
+
+@app.route('/register', methods=["POST"])
+def register():
+    if request.method == 'POST':
+       
+        email = request.json.get("email", None)
+        password = request.json.get("password", None)
+        nombre= request.json.get("nombre", None)
+        primer_apellido = request.json.get("primer_apellido", None)
+        segundo_apellido = request.json.get("segundo_apellido", None)
+
+        if not email:
+            return jsonify({"msg": "email requerido"}), 400
+        if not password:
+            return jsonify({"msg": "Password requerido"}), 400
+
+        if not nombre:
+            return jsonify({"msg": "Nombre requerido"}), 400
+
+        if not primer_apellido:
+            return jsonify({"msg": "Primer Apellido requerido"}), 400
+
+        if not segundo_apellido:
+            return jsonify({"msg": "Segundo Apellido requerido"}), 400
+
+
+        user = Usuario.query.filter_by(email=email).first()
+        if user:
+            return jsonify({"msg": "Username  already exists"}), 400
+
+        user = Usuario()
+        user.email = email
+        user.nombre = nombre
+        user.primer_apellido = primer_apellido
+        user.segundo_apellido = segundo_apellido
+
+
+
+        hashed_password = generate_password_hash(password)
+       
+        user.password = hashed_password
+
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify({"success": "Thanks. your register was successfully", "status": "true"}), 200
+
+@app.route('/login', methods=['POST'])
+def login():
+
+    if request.method == 'POST':
+        email = request.json.get("email", None)
+        password = request.json.get("password", None)
+
+        if not email:
+            return jsonify({"msg": "Username is required"}), 400
+        if not password:
+            return jsonify({"msg": "Password is required"}), 400
+
+        user = Usuario.query.filter_by(email=email).first()
+        if not user:
+            return jsonify({"msg": "Username/Password are incorrect"}), 401
+
+        if not check_password_hash(user.password, password):
+            return jsonify({"msg": "Username/Password are incorrect"}), 401
+
+        # crear el token
+        expiracion = datetime.timedelta(days=3)
+        access_token = create_access_token(identity=user.id, expires_delta=expiracion)
+
+        data = {
+            "user": user.serialize(),
+            "token": access_token,
+            "expires": expiracion.total_seconds()*1000
+        }
+        return jsonify(data), 200
+
+# @app.route("/logout", methods=["DELETE"])
+# @jwt_required()
+# def logout():
+#     jti = get_jwt()["jti"]
+#     jwt_redis_blocklist.set(jti, "", ex=ACCESS_EXPIRES)
+#     return jsonify(msg="Access token revoked")
+
+# A blocklisted access token will not be able to access this any more
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -2,18 +2,108 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+
+class Planeta(db.Model):
+    __tablename__ = 'Planeta'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
+    diametro = db.Column(db.String(250))
+    periodo_rotacion = db.Column(db.String(250))
+    periodo_orbital = db.Column(db.String(250))
+    gravedad = db.Column(db.String(250))
+    poblacion = db.Column(db.String(250))
+    clima = db.Column(db.String(250))
+    terreno= db.Column(db.String(250))
+    superfice_acuatica = db.Column(db.String(250))
+    creacion= db.Column(db.String(250))
+    editado = db.Column(db.String(250))
+    nombre = db.Column(db.String(250), nullable=False)    
+    
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "diametro": self.diametro,
+            "periodo_rotacion": self.periodo_rotacion,
+            "periodo_orbital": self.periodo_orbital,
+            "gravedad": self.gravedad,
+            "poblacion": self.poblacion,
+            "clima": self.clima,
+            "terreno": self.terreno,
+            "superfice_acuatica": self.superfice_acuatica,
+            "creacion": self.creacion,
+            "editado": self.editado,
+            "nombre": self.nombre
         }
+
+
+class Personajes(db.Model):
+    __tablename__ = 'Personajes'
+    id = db.Column(db.Integer, primary_key=True)
+    altura= db.Column(db.String(250))
+    masa = db.Column(db.String(250))
+    color_cabello = db.Column(db.String(250))
+    color_piel = db.Column(db.String(250))
+    color_ojos= db.Column(db.String(250))
+    fecha_nacimiento = db.Column(db.String(250))
+    genero = db.Column(db.String(250))
+    creacion= db.Column(db.String(250))
+    editado = db.Column(db.String(250))
+    nombre = db.Column(db.String(250))
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "altura": self.altura,
+            "masa": self.masa,
+            "color_cabello": self.color_cabello,
+            "color_piel": self.color_piel,
+            "color_ojos": self.color_ojos,
+            "fecha_nacimiento": self.fecha_nacimiento,
+            "genero": self.genero,   
+            "creacion": self.creacion,
+            "editado": self.editado,
+            "nombre": self.nombre
+            
+        }
+          
+
+class Usuario(db.Model):
+    __tablename__ = 'Usuario'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    nombre= db.Column(db.String(250))
+    primer_apellido= db.Column(db.String(250))
+    segundo_apellido= db.Column(db.String(250))
+    email=db.Column(db.String(250))
+    password=db.Column(db.String(250))
+
+    def serialize(self):
+        return {
+              "id": self.id,
+              "nombre": self.nombre,
+              "primer_apellido": self.primer_apellido,
+              "segundo_apellido": self.segundo_apellido,
+              "email": self.email,
+            
+        }
+
+class Favoritos(db.Model):
+    __tablename__ = 'Favoritos'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    planeta_id = db.Column(db.Integer,db.ForeignKey('Planeta.id'))
+    personajes_id = db.Column(db.Integer,db.ForeignKey('Personajes.id'))
+    usuario_id = db.Column(db.Integer,db.ForeignKey('Usuario.id'))
+    usuario=db.relationship(Usuario)
+    planetas=db.relationship(Planeta)
+    personajes=db.relationship(Personajes)
+
+    def serialize(self):
+        return {
+              "id": self.id,
+              "planeta_id": self.planeta_id,
+              "personajes_id": self.personajes_id,
+              "usuario_id": self.usuario_id
+        }
+
